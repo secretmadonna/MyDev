ea0b229c2633aedb4fee1d64268d1b893865bd51
secretmadonna <secretmadonna.163.com>
2016-02-25 14:05:21 +0800

    加入区域UserCenter

---
+++ MyDev/MyDev.WebSite.Portal.Web/Areas/UserCenter/UserCenterAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace MyDev.WebSite.Portal.Web.Areas.UserCenter
+{
+    public class UserCenterAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "UserCenter";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "UserCenter_default",
+                "uc/{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new string[] { "MyDev.WebSite.Portal.Web.Areas.UserCenter.Controllers" }
+            );
+        }
+    }
+}
